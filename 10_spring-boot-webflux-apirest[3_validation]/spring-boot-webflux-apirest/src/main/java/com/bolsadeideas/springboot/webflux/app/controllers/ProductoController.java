44c0de6d0a72febdfd1077d5ccca539ed8df2576
mateo_vasco <mateo.castano@sofka.com.co>
2024-05-20 14:10:17 -0500

    S5 Finished

--- 10_spring-boot-webflux-apirest[3_validation]/spring-boot-webflux-apirest/src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
+++ 10_spring-boot-webflux-apirest[3_validation]/spring-boot-webflux-apirest/src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
@@ -43,7 +43,8 @@ public class ProductoController {
         this.service = service;
     }
 
-    @PostMapping("/v2")
+	//Checked
+    @PostMapping("/create/v2")
 	public Mono<ResponseEntity<Producto>> crearConFoto(Producto producto, @RequestPart FilePart file){
 		
 		if(producto.getCreateAt()==null) {
@@ -58,42 +59,47 @@ public class ProductoController {
 		return file.transferTo(new File(path + producto.getFoto())).then(service.save(producto))
 				.map(p-> ResponseEntity
 				.created(URI.create("/api/productos/".concat(p.getId())))
-				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.contentType(MediaType.APPLICATION_JSON)
 				.body(p)
 				);
 		
 	}
-	
+
+	//Checked
 	@PostMapping("/upload/{id}")
 	public Mono<ResponseEntity<Producto>> upload(@PathVariable String id, @RequestPart FilePart file){
 		return service.findById(id).flatMap(p -> {
-			p.setFoto(UUID.randomUUID().toString() + "-" + file.filename()
+			p.setFoto(UUID.randomUUID() + "-" + file.filename()
 			.replace(" ", "")
 			.replace(":", "")
 			.replace("\\", ""));
 			
 			return file.transferTo(new File(path + p.getFoto())).then(service.save(p));
-		}).map(p -> ResponseEntity.ok(p))
+		}).map(ResponseEntity::ok)
 		.defaultIfEmpty(ResponseEntity.notFound().build());
 	}
-	
+
+	//Checked
 	@GetMapping
 	public Mono<ResponseEntity<Flux<Producto>>> lista(){
 		return Mono.just(
 				ResponseEntity.ok()
-				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.contentType(MediaType.APPLICATION_JSON)
 				.body(service.findAll())
 				);
 	}
-	
+
+	//Checked
 	@GetMapping("/{id}")
 	public Mono<ResponseEntity<Producto>> ver(@PathVariable String id){
+		System.out.println(id);
 		return service.findById(id).map(p -> ResponseEntity.ok()
-				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.contentType(MediaType.APPLICATION_JSON)
 				.body(p))
 				.defaultIfEmpty(ResponseEntity.notFound().build());
 	}
-	
+
+	//Checked
 	@PostMapping
 	public Mono<ResponseEntity<Map<String, Object>>> crear(@Valid @RequestBody Mono<Producto> monoProducto){
 		
@@ -110,28 +116,29 @@ public class ProductoController {
 				respuesta.put("timestamp", new Date());
 				return ResponseEntity
 					.created(URI.create("/api/productos/".concat(p.getId())))
-					.contentType(MediaType.APPLICATION_JSON_UTF8)
+					.contentType(MediaType.APPLICATION_JSON)
 					.body(respuesta);
 				});
 			
-		}).onErrorResume(t -> {
-			return Mono.just(t).cast(WebExchangeBindException.class)
+		}).onErrorResume(t ->
+			 Mono.just(t).cast(WebExchangeBindException.class)
 					.flatMap(e -> Mono.just(e.getFieldErrors()))
 					.flatMapMany(Flux::fromIterable)
-					.map(fieldError -> "El campo "+fieldError.getField() + " " + fieldError.getDefaultMessage())
+					.map(fieldError -> "El campo "+ fieldError.getField() + " " + fieldError.getDefaultMessage())
 					.collectList()
 					.flatMap(list -> {
 						respuesta.put("errors", list);
 						respuesta.put("timestamp", new Date());
 						respuesta.put("status", HttpStatus.BAD_REQUEST.value());
 						return Mono.just(ResponseEntity.badRequest().body(respuesta));
-					});
-							
-		});
+					})
+
+		);
 		
 
 	}
-	
+
+	//Checked
 	@PutMapping("/{id}")
 	public Mono<ResponseEntity<Producto>> editar(@RequestBody Producto producto, @PathVariable String id){
 		return service.findById(id).flatMap(p -> {
@@ -140,16 +147,16 @@ public class ProductoController {
 			p.setCategoria(producto.getCategoria());
 			return service.save(p);
 		}).map(p->ResponseEntity.created(URI.create("/api/productos/".concat(p.getId())))
-				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.contentType(MediaType.APPLICATION_JSON)
 				.body(p))
 		.defaultIfEmpty(ResponseEntity.notFound().build());
 	}
 	
 	@DeleteMapping("/{id}")
 	public Mono<ResponseEntity<Void>> eliminar(@PathVariable String id){
-		return service.findById(id).flatMap(p ->{
-			return service.delete(p).then(Mono.just(new ResponseEntity<Void>(HttpStatus.NO_CONTENT)));
-		}).defaultIfEmpty(new ResponseEntity<Void>(HttpStatus.NOT_FOUND));
+		return service.findById(id).flatMap(p ->
+			service.delete(p).then(Mono.just(new ResponseEntity<Void>(HttpStatus.NO_CONTENT)))
+		).defaultIfEmpty(new ResponseEntity<Void>(HttpStatus.NOT_FOUND));
 	}
 
 }
